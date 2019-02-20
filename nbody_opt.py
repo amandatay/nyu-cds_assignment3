6ff12d5ad9a9e3bd227eac66c19a4703c90607b1
amandatay <amandatayboonluan@gmail.com>
2019-02-21 00:45:46 +0800

    initial commit

---
+++ nbody_opt.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 20 00:10:51 2019
+
+@author: amanda
+"""
+
+"""
+    N-body simulation.
+    Task 1: Reducing function call overhead
+    Task 2: Using alternatives to membership testing of lists
+    Task 3: Using local rather than global variables
+    Task 4: Using data aggregation to reduce loop overheads
+    Original run time: --- 136.608999968 seconds ---
+    Updated  run time: --- 82.1860001087 seconds ---
+    Relative speed up: 1.66X (136.609/82.186)
+"""
+# CHANGE: Task 3
+def advance(dt, BODIES, bodies_combi):
+    '''
+        advance the system one timestep
+    '''
+    # CHANGE: Task 4
+    for body in bodies_combi: 
+        ([x1, y1, z1], v1, m1) = BODIES[body[0]]
+        ([x2, y2, z2], v2, m2) = BODIES[body[1]]
+        # CHANGE: Task 1
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2) 
+        v1[0] -= dx * m2 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        v1[1] -= dy * m2 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        v1[2] -= dz * m2 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        v2[0] += dx * m1 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        v2[1] += dy * m1 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        v2[2] += dz * m1 * dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
+        
+    for body in BODIES.keys():
+        (r, [vx, vy, vz], m) = BODIES[body] 
+        # CHANGE: Task 1
+        r[0] += dt * vx
+        r[1] += dt * vy
+        r[2] += dt * vz
+
+# CHANGE: Task 3    
+def report_energy(BODIES, bodies_combi, e=0.0):
+    '''
+        compute the energy and return it so that it can be printed
+    '''
+    # CHANGE: Task 4
+    for body in bodies_combi:
+        ([x1, y1, z1], v1, m1) = BODIES[body[0]]
+        ([x2, y2, z2], v2, m2) = BODIES[body[1]]
+        # CHANGE: Task 1
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)    
+        e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5) 
+        
+    for body in BODIES.keys():
+        (r, [vx, vy, vz], m) = BODIES[body] 
+        #CHANGE: Task 1
+        e += m * (vx * vx + vy * vy + vz * vz) / 2.
+        
+    return e
+
+# CHANGE: Task 3
+def offset_momentum(ref, BODIES , px=0.0, py=0.0, pz=0.0):
+    '''
+        ref is the body in the center of the system
+        offset values from this reference
+    '''
+    for body in BODIES.keys():
+        (r, [vx, vy, vz], m) = BODIES[body]
+        px -= vx * m
+        py -= vy * m
+        pz -= vz * m
+        
+    (r, v, m) = ref
+    v[0] = px / m
+    v[1] = py / m
+    v[2] = pz / m
+
+# CHANGE: Task 3
+def nbody(loops, reference, iterations, BODIES, bodies_combi):
+    '''
+        nbody simulation
+        loops - number of loops to run
+        reference - body at center of system
+        iterations - number of timesteps to advance
+    '''
+    # Set up global state
+    offset_momentum(BODIES[reference],BODIES)
+
+    for _ in range(loops):
+        report_energy(BODIES,bodies_combi)
+        for _ in range(iterations):
+            advance(0.01,BODIES,bodies_combi)
+        print(report_energy(BODIES,bodies_combi))
+
+import time
+import itertools
+if __name__ == '__main__':
+    
+    # CHANGE: Task 3
+    PI = 3.14159265358979323
+    SOLAR_MASS = 4 * PI * PI
+    DAYS_PER_YEAR = 365.24
+    
+    BODIES = {
+        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+    
+        'jupiter': ([4.84143144246472090e+00,
+                     -1.16032004402742839e+00,
+                     -1.03622044471123109e-01],
+                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     7.69901118419740425e-03 * DAYS_PER_YEAR,
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                    9.54791938424326609e-04 * SOLAR_MASS),
+    
+        'saturn': ([8.34336671824457987e+00,
+                    4.12479856412430479e+00,
+                    -4.03523417114321381e-01],
+                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    4.99852801234917238e-03 * DAYS_PER_YEAR,
+                    2.30417297573763929e-05 * DAYS_PER_YEAR],
+                   2.85885980666130812e-04 * SOLAR_MASS),
+    
+        'uranus': ([1.28943695621391310e+01,
+                    -1.51111514016986312e+01,
+                    -2.23307578892655734e-01],
+                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    2.37847173959480950e-03 * DAYS_PER_YEAR,
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
+                   4.36624404335156298e-05 * SOLAR_MASS),
+    
+        'neptune': ([1.53796971148509165e+01,
+                     -2.59193146099879641e+01,
+                     1.79258772950371181e-01],
+                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.62824170038242295e-03 * DAYS_PER_YEAR,
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                    5.15138902046611451e-05 * SOLAR_MASS)}
+        
+    # CHANGE: Task 4
+    bodies_combi = list(itertools.combinations(BODIES.keys(), 2)) 
+
+    start_time = time.time()
+    
+    # CHANGE: Task 3
+    nbody(100, 'sun', 20000,BODIES,bodies_combi)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
